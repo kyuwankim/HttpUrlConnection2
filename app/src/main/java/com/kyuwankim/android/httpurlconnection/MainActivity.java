7a1e04f1501b52371f90f18931bb699639c97f13
kyuwankim <lpnids@gmail.com>
2017-06-13 15:18:33 +0900

    Add setPage

--- app/src/main/java/com/kyuwankim/android/httpurlconnection/MainActivity.java
+++ app/src/main/java/com/kyuwankim/android/httpurlconnection/MainActivity.java
@@ -5,9 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements TaskInterface{
+    static final String URL_PREFIX = "http://openAPI.seoul.go.kr:8088/";
+    static final String URL_CERT   = "4c425976676b6f643437665377554c";
+    static final String URL_MID    = "/json/SearchPublicToiletPOIService/";
+    // 한 페이지에 불러오는 데이터 수
+    static final int PAGE_OFFSET = 10;
+
+    int pageBegin = 1;
+    int pageEnd = 10;
+
+
 
     TextView textView;
-    String url = "http://google.com";
+    String url = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +25,31 @@ public class MainActivity extends AppCompatActivity implements TaskInterface{
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textview);
 
-        Task.newTask(this);
+        // 최초 호출시 첫번째 집합을 불러온다.
+        setPage(1);
+
+        setUrl(pageBegin, pageEnd);
+
+        Remote.newTask(this);
+    }
+
+    private void setPage(int page){
+        pageEnd = page * PAGE_OFFSET;
+        pageBegin = pageEnd - PAGE_OFFSET + 1;
+    }
+
+    private void setUrl(int begin, int end){
+        // String
+        // StringBuffer
+        // StringBuilder
+
+        // String 연산.........
+        // String result = "문자열" + "문자열" + "문자열";
+        //                  ----------------
+        //                    메모리공간 할당
+        //                   ---------------------------
+        //                         메모리공간 할당
+        url = URL_PREFIX + URL_CERT + URL_MID +begin+"/"+end;
     }
 
     @Override
--- app/src/main/java/com/kyuwankim/android/httpurlconnection/Remote.java
+++ app/src/main/java/com/kyuwankim/android/httpurlconnection/Remote.java
@@ -1,5 +1,6 @@
 package com.kyuwankim.android.httpurlconnection;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -12,6 +13,31 @@ import java.net.URL;
  */
 
 public class Remote {
+
+    // thread 를 생성
+    public static void newTask(final TaskInterface taskInterface){
+
+        new AsyncTask<String, Void, String>(){
+            // 백그라운드 처리 함수
+            @Override
+            protected String doInBackground(String... params) {
+                String result = "";
+                try {
+                    // getData 함수로 데이터를 가져온다.
+                    result = getData(params[0]);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return result;
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                // 결과처리
+                taskInterface.postExecute(result);
+            }
+        }.execute(taskInterface.getUrl());
+    }
+
     // 인자로 받은 url 로 네트웍을 통해 데이터를 가져오는 함수
     public static String getData(String url) throws Exception {  // <- 요청한 곳에서 Exception 처리를 해준다.
         String result = "";
